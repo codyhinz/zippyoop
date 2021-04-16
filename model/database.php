c833cc1d396553cc435e063b10d04a56c33b79cf
Cody Hinz <codyhinz@gmail.com>
2021-04-16 14:01:27 -0400

    Updating for OOP

--- model/database.php
+++ model/database.php
@@ -1,11 +1,21 @@
 <?php 
 
-    $dsn = 'mysql:host=hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vifmi5ekcwb44w9l';
-    $username = 'm2hdc19rf2wturb7';
-    $password = 'rkxk5ubohcqjuvfz';
+    class Database {
+        private static $dsn = 'mysql:host=hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vifmi5ekcwb44w9l';
+        private static $username = 'm2hdc19rf2wturb7';
+        private static $password = 'rkxk5ubohcqjuvfz';
+        private static $db;
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch(PDOException $e) {
-        echo $e->getMessage();
+        private function __construct() {}
+
+        public static function getDB() {
+            if(!isset(self::$db)) {
+                try {
+                    self::$db = new PDO(self::$dsn, self::$username, self::$password);
+                } catch(PDOException $e) {
+                    echo $e->getMessage();
+                }
+            }
+            return self::$db;
+        }
     }
